991cd90461b93c467a56dca64c2962ea4c1f46ad
Allan Karlson <beesforever@gmx.de>
2021-05-26 18:00:11 +0200

    image capture

---
+++ image_capture.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+os.system("raspistill -o rasberryimage.png")
+
+
+
+url = "https://api.gardsfabriken.de/api/v1/farm/file?type=shed_cam"
+
+payload={}
+files=[
+  ('file',('Shed_Image' ,open('/home/pi/scripts/rasberryimage.png','rb'),'image/png'))
+]
+headers = {}
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+os.system("rm rasberryimage.png")
+
+print(response.text)
+
+
